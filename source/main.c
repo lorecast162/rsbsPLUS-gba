01ae6a9190e7e5e8a9230ac55b235fe2d5c8907f
lorecast162 <lorecastcauli@gmail.com>
2020-09-10 11:55:59 +0200

    Added code comments

--- source/main.c
+++ source/main.c
@@ -1,32 +1,40 @@
 //SPDX-License-Identifier: BSD-3-Clause
 //SPDX-FileCopyrightText: 2020 Lorenzo Cauli (lorecast162)
 
+//enable multiboot?
 #define MULTIBOOT int __gba_multiboot;
 MULTIBOOT
 
 typedef unsigned short u16;
 
+//include spheres data and palette
 #include "sphere.h"
 #include "blauPalette.h"
 #include "gruenPalette.h"
 
+//display mode setting macro
 #define SetMode(mode) REG_DISPCNT = (mode)
 
+//buttons register
 volatile unsigned int *BUTTONS = (volatile unsigned int *)0x04000130;
 
+//video status registers
 #define REG_DISPCNT *(volatile unsigned short*)0x4000000
 #define BGPaletteMem ((unsigned short*)0x5000000)
 #define REG_VCOUNT *(volatile unsigned short*)0x4000006
 #define REG_DISPSTAT *(volatile unsigned short*)0x4000004
 
+//sprite registers
 #define SpriteMem  ((unsigned short*)0x7000000)
 #define SpriteData ((unsigned short*)0x6010000)
 #define SpritePal  ((unsigned short*)0x5000200)
 
+//object flags
 #define OBJ_MAP_2D 0x0
 #define OBJ_MAP_1D 0x40
 #define OBJ_ENABLE 0x1000
 
+//various sprite flags
 #define ROTATION_FLAG 0x100
 #define SIZE_DOUBLE 0x200
 #define MODE_NORMAL 0x0
@@ -38,7 +46,6 @@ volatile unsigned int *BUTTONS = (volatile unsigned int *)0x04000130;
 #define SQUARE 0x0
 #define TALL 0x4000
 #define WIDE 0x8000
-
 #define ROTDATA(n) ((n) << 9)
 #define HORIZONTAL_FLIP 0x1000
 #define VERTICAL_FLIP 0x2000
@@ -50,6 +57,7 @@ volatile unsigned int *BUTTONS = (volatile unsigned int *)0x04000130;
 #define PRIORITY(n) ( (n) << 10 )
 #define PALETTE(n) ( (n) << 12 )
 
+//sprite attributes struct
 typedef struct tagSprite {
 	unsigned short attribute0;
 	unsigned short attribute1;
@@ -59,6 +67,7 @@ typedef struct tagSprite {
 
 Sprite sprites[128];
 
+//gba buttons
 #define BTN_UP 64
 #define BTN_DOWN 128
 #define BTN_LEFT 32
@@ -68,8 +77,8 @@ void WaitForVBlank(void);
 void UpdateSpriteMemory(void);
 
 int main(void) {
-	signed short x = 10, y = 40;
-	signed short xdir = 1, ydir = 1;
+	signed short x = 88, y = 48;
+	signed short xinc = 1;
 
 	int char_number = 0;
 	int n;
@@ -77,40 +86,49 @@ int main(void) {
 	int curPalette = 0;
 	int prevPalette = curPalette;
 
+	//set video mode 2 enable objects and map them 1D in memory
 	SetMode(2 | OBJ_ENABLE | OBJ_MAP_1D);
 
+	//set all sprites to be at bottom right corner
 	for (n = 0; n < 128; n++) {
 		sprites[n].attribute0 = 160;
 		sprites[n].attribute1 = 240;
 	}
 
+	//copy sprites palette
 	for( n = 0; n < 256; n++) {
 		SpritePal[n] = spherePalette[n];
 	}
 
+	//copy red sphere data
 	for (n = 0; n < 256*8; n++) {
 		SpriteData[n] = sphereData[n];
 	}
 
+	//give it attributes
 	sprites[0].attribute0 = COLOR_256 | y;
 	sprites[0].attribute1 = SIZE_64 | x;
 	sprites[0].attribute2 = char_number;
 
 	while(1) {
+		//set its x coord
 		sprites[0].attribute1 = SIZE_64 | x;
 
 		WaitForVBlank();
 
+		//check if buttons were pressed
 		if (*BUTTONS) {
+			//switch palette var
 			if (!(*BUTTONS & BTN_UP)) curPalette = 1;
 			else if (!(*BUTTONS & BTN_DOWN)) curPalette = 2;
 			else curPalette = 0;
 
 			//horiz movement
-			if (!(*BUTTONS & BTN_LEFT) && x >= 0) x -= 1;
-			else if (!(*BUTTONS & BTN_RIGHT) && (x + sphere_WIDTH) <= 240) x += 1;
+			if (!(*BUTTONS & BTN_LEFT) && x >= 0) x -= xinc;
+			else if (!(*BUTTONS & BTN_RIGHT) && (x + sphere_WIDTH) <= 240) x += xinc;
 		}
 
+		//check if palette var was changed to avoid copying same stuff over and over again
 		if (curPalette != prevPalette) {
 			switch (curPalette) {
 				case 0:
